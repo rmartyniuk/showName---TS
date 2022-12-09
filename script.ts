0f4a78c29f0b979a9bd31758ec8e1c672c236647
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-12-09 14:39:24 +0100

    add questions

--- script.ts
+++ script.ts
@@ -5,24 +5,26 @@ function showMyName(name: string) {
 showMyName('John')
 
 //dlaczego również poza nawiasem należało wpisać  ": number"???
+//odpowiedź: w celu określenia typu zwracanych danych
 function square(a: number): number {
   return a * a
 }
 
 square(10)
 
+//this wskazuje na kontekst wykonywania funkcji. this nie zostanie potraktowane jako prawdziwy parametr funkcji ani nie zmieni kolejności paramterów. Więc właściwie w jakim celu go tu należy użyć?
 function sum(
   this: any,
   title: string,
+
+  //Czy "...numbers" oznacza wiele elementów typu liczbowego, które mają się znajdować w tablicy? Co oznaczają/na co wskazują te trzy kropeczki?
   ...numbers: number[]
 ) {
   return (title +
     ' = ' +
     numbers.reduce(
-      (sum: number,
-        num: number): number =>
-        sum + num,
-      0
+      (sum: number, num: number): number =>
+        sum + num, 0
     )
   );
 }
@@ -40,7 +42,7 @@ const executeFunc = (
   const age = 24;
   const hobbies = ['sport', 'Movies'];
   return func(title, age, hobbies);
-};
+}
 
 executeFunc((title, age, hobbies) => {
   return true;
